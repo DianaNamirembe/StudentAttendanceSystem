e3019c7c9c51464ab0beba73e3ebb4358c73c81b
DianaNamirembe <diana.namirembe@hotmail.com>
2026-03-31 22:16:33 +0300

    Changes in views

--- attendance_system/attendance/views.py
+++ attendance_system/attendance/views.py
@@ -4,14 +4,14 @@ from .models import Student
 # Create your views here.
 def student_create(request):
     if request.method == "POST":
-        # 1. Grab the data from the HTML names we defined above
+       
         name_from_html = request.POST.get('student_name')
         id_from_html = request.POST.get('sid')
-        # Checkboxes are tricky: they only exist in POST if they are checked
+       
         is_present_from_html = 'present' in request.POST 
         date_from_html = request.POST.get('attendance_date')
 
-        # 2. Create and Save the student object to the database
+        
         Student.objects.create(
             name=name_from_html,
             student_id=id_from_html,
@@ -19,19 +19,19 @@ def student_create(request):
             date=date_from_html
         )
 
-        # 3. Go back to the list
+        
         return redirect('student_list')
 
     return render(request, 'attendance/student_form.html')
 
-    # 1. READ (List View / Homepage)
+    
 def student_list(request):
     all_students = Student.objects.all()
     return render(request, 'attendance/student_list.html', {'students': all_students})
 
 # 2. READ (Detail View)
 def student_detail(request, pk):
-    # This finds one specific student using their ID (pk)
+    
     single_student = Student.objects.get(id=pk)
     return render(request, 'attendance/student_detail.html', {'student': single_student})
 
